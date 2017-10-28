d82e5de9cc1a8bd1e8c31973eb869b018c613c55
savov <savov@hotmail.com>
2017-10-28 12:26:36 -0700

    Functioning example with the help from Fabian Hueske (Data Artisans)

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,12 +1,12 @@
 import java.util.Properties
 
+import org.apache.flink.api.scala._
+import org.apache.flink.streaming.api.TimeCharacteristic
+import org.apache.flink.streaming.api.functions.AssignerWithPeriodicWatermarks
 import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
+import org.apache.flink.streaming.api.watermark.Watermark
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer010
 import org.apache.flink.streaming.util.serialization.SimpleStringSchema
-import org.apache.flink.api.scala._
-import org.apache.flink.api.common._
-import org.apache.flink.streaming.api.TimeCharacteristic
-import org.apache.flink.streaming.api.windowing.time.Time
 import org.apache.flink.table.api.TableEnvironment
 import org.apache.flink.table.api.scala._
 
@@ -18,39 +18,43 @@ object Main extends App{
   properties.setProperty("group.id", "test")
 
   // using 0.10 consumer, so event time should come free in the stream
-  val consumer = new FlinkKafkaConsumer010[String]("test", new SimpleStringSchema, properties)
+  val consumer = new FlinkKafkaConsumer010("test", new SimpleStringSchema, properties)
   consumer.setStartFromEarliest()
 
   val env = StreamExecutionEnvironment.getExecutionEnvironment
   val tenv = TableEnvironment.getTableEnvironment(env)
-  env.enableCheckpointing(5000)
 
-  // Following https://ci.apache.org/projects/flink/flink-docs-release-1.3/dev/connectors/kafka.html#using-kafka-timestamps-and-flink-event-time-in-kafka-010
-  // looks like the 0.10 consumer it will emit the event time timestamps as a data field.   How to use it in
-  // time windows?
   env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime)
+
+  class KafkaAssigner[T] extends AssignerWithPeriodicWatermarks[T] {
+    var maxTs = 0L
+    override def extractTimestamp(element: T, previousElementTimestamp: Long): Long = {
+      maxTs = Math.max(maxTs, previousElementTimestamp)
+      previousElementTimestamp
+    }
+    override def getCurrentWatermark: Watermark = new Watermark(maxTs - 1L)
+  }
 
   val stream = env
     .addSource(consumer)
-    .flatMap(_.split("\\s"))
+    .assignTimestampsAndWatermarks(new KafkaAssigner[String])
+    .flatMap(_.split("\\W+").filter(_.nonEmpty))
+    // Wrapping in a Tuple to workaround a bug in Flink 1.3.2.
+    // See https://stackoverflow.com/questions/46987246/flinkkafka-0-10-how-to-extract-kafka-message-timestamps-as-values-in-the-strea
+    .map(w => Tuple1(w))
 
-  // Where is the DSL for fields Expression* objects like 'w described?
-  // None of the examples have the 'import org.apache.flink.table.api.scala._' showing
-  // or reference to the DSL syntax
-  tenv.registerDataStream("Words", stream, 'w)
+  val tbl = tenv.fromDataStream(stream, 'w, 'ts.rowtime)
+  tenv.registerTable("Words", tbl)
 
-  // Basically following: https://flink.apache.org/news/2017/04/04/dynamic-tables.html
-  // How do I extract the event time which FlinkKafkaConsumer010 is supposed to provide?
-  //  'rowtime' is not found (not clear from the example on the page where it comes from)
-  // Can be interpreted as a keyword, which it is not.
   val res = tenv.sql(
     """
       | select w, count(*)
       | from Words
-      | group by tumble(rowtime, interval '1' second)
+      | group by w, tumble(ts, interval '5' second)
     """.stripMargin
   )
+
   res.toAppendStream[(String, Long)].print
 
-  env.execute("andrey-job")
+  env.execute("wordcount-job")
 }
